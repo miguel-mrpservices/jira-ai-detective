8115027d9f15f76761ca9827461925a3227f44b2
Miguel <miguel@mrpservices.tech>
2026-05-03 00:50:10 +0200

    initial setup fmt fix

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,8 +1,8 @@
 output "jira_webhook_url" {
   description = "Copy this URL and paste it into Jira Webhook settings"
   # We join the URL of the Lambda with the value of the variable
-  value       = "${aws_lambda_function_url.agent_url.function_url}?token=${var.webhook_secret_token}"
-  sensitive   = true
+  value     = "${aws_lambda_function_url.agent_url.function_url}?token=${var.webhook_secret_token}"
+  sensitive = true
 }
 
 # AWS Region output for easy script configuration
